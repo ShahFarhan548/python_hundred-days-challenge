cb8abd3973eda25b796dbd5f8a176fee7b8e8455
ShahFarhan548 <farhaanshah336@gmail.com>
2022-08-22 07:50:10 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var car = {
+    make : 'volvox',
+    speed: 120
+    drive: function (){
+        return "hello jolla";
+    }
+    engine : {
+        size: 2.0,
+        make : "bow",
+        fuel : "petrol",
+        pistons: [ {maker: "BMW"},"birgaa"]
+    }
+};
+console.log(car.speed);
